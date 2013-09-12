c34f2127e7d2ca93d4f8452aa1040406b054148a
DevBox <devbox@cloudcollege.com>
2013-09-12 14:59:04 -0300

    tweaked settings

--- server.js
+++ server.js
@@ -4,9 +4,21 @@ var linksnip = require('linksnip')
 app = express()
 
 app.get('/', function(req, res) {
-    linksnip(req.query.url, function(data) {
-        res.send(data);
-    })
+    url = req.query.url;
+    console.log("Received request for " + url)
+    try {
+        linksnip(url, function(data) {
+            console.log(data)
+            console.log("Successfully parsed " + url)
+            res.json(data);
+        })
+    } catch(e) {
+        console.log(e)
+    }
 })
 
-app.listen(9050)
+port = process.env.PORT || 9050
+
+app.listen(port, function() {
+    console.log("Listening on " + port)
+})
